427fe5e45e78dc28569aa5c7e3bf60760103fc51
Sahil R <sahilrw000@gmail.com>
2023-10-28 10:51:39 +0530

    displayed all  the pieces on the screen

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ class Main:
 
         while True:
             game.show_bg(screen)
+            game.show_pieces(screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
