085a6f0aa18232df4c704897916525e79dc4f822
Sahil R <sahilrw000@gmail.com>
2023-11-20 11:36:06 +0530

    change themes function and sound function added. set the basis for game reset functionality

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@ from const import *
 from board import Board
 from dragger import Dragger
 from config import Config
+from square import Square
 
 
 class Game:
@@ -26,6 +27,26 @@ class Game:
                 rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
                 # blit
                 pygame.draw.rect(surface, color, rect)
+
+                # row coordinates
+                if col == 0:
+                    # color
+                    color = theme.bg.dark if row % 2 == 0 else theme.bg.light
+                    # label
+                    lbl = self.config.font.render(str(ROWS - row), 1, color)
+                    lbl_pos = (5, 5 + row * SQSIZE)
+                    # blit
+                    surface.blit(lbl, lbl_pos)
+
+                # col coordinates
+                if row == 7:
+                    # color
+                    color = theme.bg.dark if (row + col) % 2 == 0 else theme.bg.light
+                    # label
+                    lbl = self.config.font.render(Square.get_alphacol(col), 1, color)
+                    lbl_pos = (col * SQSIZE + SQSIZE - 20, HEIGHT - 20)
+                    # blit
+                    surface.blit(lbl, lbl_pos)
 
     def show_pieces(self, surface):
         for row in range(ROWS):
@@ -113,3 +134,9 @@ class Game:
 
     def change_theme(self):
         self.config.change_theme()
+
+    def play_sound(self, captured=False):
+        if captured:
+            self.config.capture_sound.play()
+        else:
+            self.config.move_sound.play()
